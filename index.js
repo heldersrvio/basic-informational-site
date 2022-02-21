aac7542dbb9d464029feb9f293823273f5f3f803
Helder Sérvio <heldersrvio@gmail.com>
2022-02-21 20:44:25 -0300

    FEAT: FILL HTML PAGES, ADD BASIC STYLING

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 
-const sendContentsOfFile = (filePath, res, statusCode = 200) => {
+const sendContentsOfFile = (
+	filePath,
+	res,
+	statusCode = 200,
+	contentType = 'text/html'
+) => {
 	fs.readFile(filePath, (_err, data) => {
 		res.statusCode = statusCode;
-		res.setHeader('Content-Type', 'text/html');
+		res.setHeader('Content-Type', contentType);
 		res.end(data);
 	});
 };
@@ -17,6 +22,8 @@ http
 			sendContentsOfFile('./about.html', res);
 		} else if (req.url === '/contact-me') {
 			sendContentsOfFile('./contact-me.html', res);
+		} else if (req.url === '/styles.css') {
+			sendContentsOfFile('./styles.css', res, 200, 'text/css');
 		} else {
 			sendContentsOfFile('./404.html', res, 404);
 		}
